4f1ee642cf3f604233271913eceec92b1d814e5b
Sumanta Kundu <sumantasam1990@gmail.com>
2021-10-15 23:41:55 +0530

    from mac 15th oct, 21 at midnight
    some mobile version fixed

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -18,8 +18,9 @@
               <a class="navbar-brand" href="/">
                   <img src="{{ asset('images/logo.webp') }}" >
               </a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+              <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+{{--                <span class="navbar-toggler-icon"></span>--}}
+                  <i class="bi bi-list"></i>
               </button>
               <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mx-auto">
