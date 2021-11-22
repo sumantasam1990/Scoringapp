38b381606faf01072cfbe99d1e25ffccf9e66a2b
Sumanta Kundu <sumantasam1990@gmail.com>
2021-11-22 23:02:44 +0530

    from mac

--- resources/views/pages/howitworksbuyer.blade.php
+++ resources/views/pages/howitworksbuyer.blade.php
@@ -14,27 +14,32 @@
                 </div>
                 <div class="col-md-12 text-center mt-4">
                     <div class="circle-number">2</div>
-                    <h4 class="mt-3 display-6 fw-bold">Invite buyers and listing agents</h4>
-                    <p>In your Dashboard, simply click "Invite Listing Agent" or “Invite Buyer”. Add their name, and email to send the invite.</p>
+                    <h4 class="mt-3 display-6 fw-bold">Create A Score Page</h4>
+                    <p>In your Dashboard, simply click "Create A Score Page". Each Score Page is a dedicated page for one individual buyer or a group of buyers.</p>
                 </div>
                 <div class="col-md-12 text-center mt-6">
                     <div class="circle-number">3</div>
-                    <h4 class="mt-3 display-6 fw-bold">Add Properties</h4>
-                    <p>Add properties for your buyers to score. Whenever you add a new property, your buyers will receive an email, asking them to score that property.</p>
+                    <h4 class="mt-3 display-6 fw-bold">Invite Buyers & Listing Agents</h4>
+                    <p>In your Dashboard, simply click "Invite Listing Agent" and "Invite Buyer". Add their name, and email to send the invite.</p>
                 </div>
                 <div class="col-md-12 text-center mt-6">
                     <div class="circle-number">4</div>
-                    <h4 class="mt-3 display-6 fw-bold">View Added Properties</h4>
-                    <p>View new properties that either your buyers and/or listing agents have added.
+                    <h4 class="mt-3 display-6 fw-bold">Add Properties</h4>
+                    <p>Add properties for your buyers to score. Whenever you add a new property, your buyers will receive an email, asking them to score that property.
                     </p>
                 </div>
                 <div class="col-md-12 text-center mt-6">
                     <div class="circle-number">5</div>
+                    <h4 class="mt-3 display-6 fw-bold">View Added Properties</h4>
+                    <p>View new properties that either your buyers and/or listing agents have added.</p>
+                </div>
+                <div class="col-md-12 text-center mt-6">
+                    <div class="circle-number">6</div>
                     <h4 class="mt-3 display-6 fw-bold">Messages</h4>
                     <p>Within each Score Page, you, your buyers and listing agents can create as many Threaded Messages as possible. This allows detailed discussions to happen about not only each property itself but also each criteria and score for each property. Add important dates and notes as well.</p>
                 </div>
                 <div class="col-md-12 text-center mt-6">
-                    <div class="circle-number">6</div>
+                    <div class="circle-number">7</div>
                     <h4 class="mt-3 display-6 fw-bold">View Scores & Questionnaire</h4>
                     <p>View the scores that your buyers have added to each property and each criteria. Use these scores to show your buyers more relevant properties based on their detailed feedback. Best of all, each buyer can add their own score to each property. View the buyers questionnaire to view the exact aspects of a house that the buyers are looking for.</p>
                 </div>
